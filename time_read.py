1a89369cdf846aa56f29a87caf4d1a658e2b579c
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-23 16:19:01 +0000

    prepped 4 test

--- time_read.py
+++ time_read.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
 import time
 from astropy.table import Table
+import random
 
 t0 = time.time()
-c = Table.read("/Users/Harry/M101/catalogs_sdss/sdss_dr12_testfile_copy.csv")
+c = Table.read(join("/share/data1/SDSS_DR12_Photometry", random.choice(listdir("/share/data1/SDSS_DR12_Photometry"))))
 t1 = time.time() - t0
 
 print("Table reading time:", t1)
