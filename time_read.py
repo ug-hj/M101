7beebc0c4c3c1b7e333c431aa88ed79d44bcf46f
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-23 17:15:29 +0000

    more stupidity...

--- time_read.py
+++ time_read.py
@@ -3,6 +3,7 @@ import time
 from astropy.table import Table
 import random
 from os.path import join
+from os import listdir
 
 t0 = time.time()
 c = Table.read(join("/share/data1/SDSS_DR12_Photometry", random.choice(listdir("/share/data1/SDSS_DR12_Photometry"))))
