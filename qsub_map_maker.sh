e033da929ccfb35fd011d61d6d6b708775550e81
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-24 00:15:41 +0000

    more cores...

--- qsub_map_maker.sh
+++ qsub_map_maker.sh
@@ -1,7 +1,7 @@
 #!/bin/tcsh
 #PBS -q smp
 #PBS -N dr12.map
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=4
 #PBS -l mem=100gb
 #PBS -l walltime=120:00:00
 #PBS -M zcaphjo@ucl.ac.uk
@@ -11,4 +11,4 @@ module load dev_tools/nov2014/python-anaconda
 
 cd $PBS_O_WORKDIR
 
-time python /share/splinter/ug_hj/multimap_n_merge.py
+time python /share/splinter/ug_hj/M101/make_maps.py
