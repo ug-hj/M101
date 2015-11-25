4ad7507ea9d8f92f4dfed1894a65d0d8c857dc22
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-25 23:18:40 +0000

    merge

---
+++ merge.py
@@ -0,0 +1,13 @@
+#%matplotlib
+import os
+import os.path
+import healpy as hp
+
+npix = hp.nside2npix(128)
+hmap = np.zeros(npix)
+
+for cmap in os.listdir("/share/splinter/ug_hj/M101/128_SDSS"):
+    m = hp.read_map(os.path.join("/share/splinter/ug_hj/M101/128_SDSS", cmap))
+    hmap += m
+    
+hp.write_map("/share/splinter/ug_hj/M101/128_SDSS/SDSS_cmap_128.fits", hmap)
