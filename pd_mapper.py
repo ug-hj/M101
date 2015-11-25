5d687905d8a165107c3a74b6a576db537430fa8a
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-25 15:28:44 +0000

    finally...?

--- pd_mapper.py
+++ pd_mapper.py
@@ -14,7 +14,7 @@ def mapper1(catalog_dir, nside, ra_col, dec_col, out_dir):
     npix = hp.nside2npix(nside)
     
     for cat in listdir(catalog_dir):
-        if cat.endswith(".csv"): #and cat.startswith("with_header"):
+        if cat.endswith(".csv") and cat.startswith("with_header"):
             # read catalog
             c = pandas.read_csv(join(catalog_dir, cat), sep=',', low_memory=False, header=0, dtype={ra_col : np.float64, dec_col : np.float64}, engine=None, usecols=[1,2])
             ra = c["ra"]
