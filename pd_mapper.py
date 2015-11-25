3d6d592ef3885ad1522c880aebb480ee4550855b
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-25 18:22:55 +0000

    seg fault, trying diff pd arg...

--- pd_mapper.py
+++ pd_mapper.py
@@ -16,7 +16,7 @@ def mapper1(catalog_dir, nside, ra_col, dec_col, out_dir):
     for cat in listdir(catalog_dir):
         if cat.endswith(".csv") and cat.startswith("with_header"):
             # read catalog
-            c = pandas.read_csv(join(catalog_dir, cat), sep=',', low_memory=False, header=0, dtype={ra_col : np.float64, dec_col : np.float64}, engine=None, usecols=[1,2])
+            c = pandas.read_csv(join(catalog_dir, cat), sep=',', header=0, dtype={ra_col : np.float64, dec_col : np.float64}, engine=None, usecols=[1,2])
             ra = c["ra"]
             dec = c["dec"]
             print("table columns read")
