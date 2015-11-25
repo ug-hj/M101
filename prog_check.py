a44a1723544b6b3c51769048329e5bb46a3b80a1
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-11-25 13:42:49 +0000

    checking progress w maps

---
+++ prog_check.py
@@ -0,0 +1,25 @@
+from __future__ import division
+import numpy as np
+import healpy as hp
+import matplotlib
+import random
+from os.path import join, normpath, basename, isdir
+from os import listdir, mkdir
+from astropy.table import Table
+import gc
+
+out_dir = "/share/splinter/ug_hj/128_SDSS"
+nside = 128
+out_dir = "/share/splinter/ug_hj/128_SDSS"
+
+assert hp.isnsideok(nside), ("nside must be a power of 2")
+npix = hp.nside2npix(nside)
+hmap = np.zeros(hp.nside2npix(nside))
+
+for cmap in listdir(out_dir):
+	m = hp.read_map(join(out_dir, cmap))
+	hmap += m
+
+out_filename = str(nside) + "PRELIMcmap.fits"
+f = join(out_dir, out_filename)
+hp.write_map(f, hmap)
