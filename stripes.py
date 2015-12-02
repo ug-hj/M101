d515cd4389f3acffb2fec6723cabfe6fba899ff2
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-12-02 23:23:14 +0000

    DISPLAY var fix...?

--- stripes.py
+++ stripes.py
@@ -2,6 +2,7 @@ from __future__ import print_function, division
 import numpy as np
 import healpy as hp
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import random
 from os.path import join, normpath, basename, isdir
