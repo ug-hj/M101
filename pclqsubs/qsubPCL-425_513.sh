fc6407161a784675cd217b49c4b950d9badc7dd4
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-12-16 14:33:44 +0000

    parallelising job

---
+++ pclqsubs/qsubPCL-425_513.sh
@@ -0,0 +1,18 @@
+#!/bin/tcsh
+#PBS -q compute
+#PBS -N PCL
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=50gb
+#PBS -l walltime=120:00:00
+#PBS -M zcaphjo@ucl.ac.uk
+#PBS -m bea
+
+source /share/splinter/sbalan/Projects/LauraPCL/envvars.csh
+
+cd $PBS_O_WORKDIR
+
+IlmJlm -m /share/splinter/ug_hj/M101/256cutmapValuemask.fits -O /share/splinter/ug_hj/M101/PCL/256cutmapValuemask_IlmJlm.dat -N 256 -l 425 -L 513
+
+Map2Alm -I /share/splinter/ug_hj/M101/256cutmap.fits -O /share/splinter/ug_hj/M101/PCL/256cutmap_alms.fits -L 513 -m /share/splinter/ug_hj/M101/256cutmapValuemask.fits
+
+Alm2Cl -I /share/splinter/ug_hj/M101/PCL/256cutmap_alms.fits -O /share/splinter/ug_hj/M101/PCL/256cutmap_Cls.dat -P -m /share/splinter/ug_hj/M101/256cutmapValuemask.fits -o -R /share/splinter/ug_hj/M101/PCL/256cutmapValuemask_IlmJlm.dat -T /share/splinter/ug_hj/M101/256cutmap.fits -N 256 -L 513
