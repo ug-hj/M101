049393b02532a49350bafe91000376efaeec8aaf
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-12-20 21:22:02 +0000

    ..

--- batch_qsub.py
+++ batch_qsub.py
@@ -2,5 +2,5 @@ import os
 
 for q in os.listdir("/share/splinter/ug_hj/M101/PCL/bin10/"):
 	if q.endswith(".sh"):
-		os.system("qsub" + q)
+		os.system("qsub" + "/share/splinter/ug_hj/M101/PCL/bin10/" + q)
 		os.system("sleep 1")
