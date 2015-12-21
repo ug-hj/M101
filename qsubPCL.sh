771e0751d89ee2557c22a40e07f9e58e0092fdfd
Harry Johnston <ug_hj@splinter-login.local>
2015-12-21 16:25:48 +0000

    l_bin = 85 Cl w/ Value mask only

--- qsubPCL.sh
+++ qsubPCL.sh
@@ -11,8 +11,4 @@ source /share/splinter/sbalan/Projects/LauraPCL/envvars.csh
 
 cd $PBS_O_WORKDIR
 
-#IlmJlm -m /share/splinter/ug_hj/M101/256cutmapValuemask.fits -O /share/splinter/ug_hj/M101/PCL/256cutmapValuemask_IlmJlm.dat -N 256 -L 513
-
-#Map2Alm -I /share/splinter/ug_hj/M101/256cutmap.fits -O /share/splinter/ug_hj/M101/PCL/256cutmap_alms.fits -L 513 -m /share/splinter/ug_hj/M101/256cutmapValuemask.fits
-
 Alm2Cl -I /share/splinter/ug_hj/M101/PCL/256cutmap_alms.fits -O /share/splinter/ug_hj/M101/PCL/bin85/256cutmap_Cls.dat -P -m /share/splinter/ug_hj/M101/256cutmapValuemask.fits -o -R /share/splinter/ug_hj/M101/PCL/bin85/256Valmask_IlmJlm_tot.dat -T /share/splinter/ug_hj/M101/256cutmap.fits -N 256 -L 513
