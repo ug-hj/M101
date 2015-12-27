0c6512d13b8b1a81b99997a511c94d489e5a20bc
Harry Johnston <zcaphjo@ucl.ac.uk>
2015-12-27 17:50:24 +0000

    alms qsub

---
+++ qsubalms.sh
@@ -0,0 +1,13 @@
+#!/bin/tcsh
+#PBS -q compute
+#PBS -N Cl.4mask
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=50gb
+#PBS -l walltime=120:00:00
+#PBS -M zcaphjo@ucl.ac.uk
+
+source /share/splinter/sbalan/Projects/LauraPCL/envvars.csh
+
+cd $PBS_O_WORKDIR
+
+Map2Alm -I /share/splinter/ug_hj/M101/256cutmap.fits -O /share/splinter/ug_hj/M101/PCL/ValMaxBriBadx1/256cutmap_alms.fits -L 513 -m /share/splinter/ug_hj/M101/256ValMaxBriBadx1_mask.fits
