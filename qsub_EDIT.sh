975ae5e772f779f7aa2a69950a83c30054f73a4a
Harry Johnston <ug_hj@splinter-login.local>
2016-02-11 18:33:40 +0000

    vi

--- qsub_EDIT.sh
+++ qsub_EDIT.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 #PBS -q compute
-#PBS -N PZ_merge
+#PBS -N test
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=20gb
 #PBS -l walltime=120:00:00
@@ -11,4 +11,4 @@ module load dev_tools/nov2014/python-anaconda
 
 cd $PBS_O_WORKDIR
 
-time python /share/splinter/ug_hj/M101/spl_Merge.py
+time python /share/splinter/ug_hj/M101/duplicates.py
